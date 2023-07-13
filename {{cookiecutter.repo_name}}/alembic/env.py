37d30dc5e20ecff7ab25a908c54b03d0d71320df
Luka <luka.vuksanovic35@gmail.com>
2023-07-13 13:35:50 +0200

    Alembic parameters changed to follow settings class

--- {{cookiecutter.repo_name}}/alembic/env.py
+++ {{cookiecutter.repo_name}}/alembic/env.py
@@ -2,6 +2,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.models.base import Base
+from app.models.user import *
 from alembic import context
 from app.settings import settings
 
