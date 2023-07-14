21a1cf2e007d2594d3e7fb72cccaadf313d279ea
Luka Vuksanović <46270806+Lolomgrofl@users.noreply.github.com>
2023-07-14 13:06:19 +0200

    Settings parameters renaming (#8)
    * init files and tests folder
    
    * Changing settings SQLAlchemy name to PostgresURL

--- {{cookiecutter.repo_name}}/alembic/env.py
+++ {{cookiecutter.repo_name}}/alembic/env.py
@@ -10,7 +10,7 @@ from app.settings import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_URL)
+config.set_main_option("sqlalchemy.url", settings.POSTGRES_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
