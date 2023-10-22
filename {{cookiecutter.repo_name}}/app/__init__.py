6bd86c62b1c5546d3a1c76a6447ec7df66767508
Luka <luka.vuksanovic35@gmail.com>
2023-10-22 23:51:10 +0200

    semantic versioning

---
+++ {{cookiecutter.repo_name}}/app/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.0"
+__author__ = ""
+
+# MAJOR.MINOR.PATCH
+# MAJOR version when you make incompatible API changes,
+# MINOR version when you add functionality in a backwards compatible manner, and
+# PATCH version when you make backwards compatible bug fixes.
+# Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+# For more information, see https://semver.org/.
